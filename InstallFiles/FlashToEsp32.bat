7344fc6875eb42f336063349659aac382cf38fd9
Eric Gunnerson <eric_gunnerson@hotmail.com>
2021-08-28 17:50:51 -0700

    Added directions to use esptool.py to flash

---
+++ InstallFiles/FlashToEsp32.bat
@@ -0,0 +1,1 @@
+esptool.py --chip esp32 --port "COM4" --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 bootloader_dio_40m.bin 0x8000 partitions.bin 0xe000 boot_app0.bin 0x10000 firmware.bin 
--- UpdateReleaseFiles.bat
+++ UpdateReleaseFiles.bat
@@ -1,3 +1,3 @@
 copy code\WinFadeInstaller\release\winfadeinstaller.msi InstallFiles\
-copy Code\SequenceController\.pio\build\esp32doit-devkit-v1\firmware.* InstallFiles\
+copy Code\SequenceController\.pio\build\esp32doit-devkit-v1\*.bin InstallFiles\
 copy \data\websites\EagleDecorations\Fade* docs\
